67383d092555b0f9fe22ee661e277636fe27969f
Ash-Rahman <4rahma75@solent.ac.uk>
2019-11-13 09:31:41 +0000

    week 6-7

---
+++ myPracticeCourseWork/week7/mocktca/library/model/src/main/java/solent/ac/uk/ood/examples/exam/model/RestInterface.java
@@ -0,0 +1,11 @@
+package solent.ac.uk.ood.examples.exam.model;
+
+public interface RestInterface {
+
+    public ReplyMessage retrieveMatchingBooks(Book bookTempate);
+    
+    public ReplyMessage retrieveBook(Integer id);
+    
+    public ReplyMessage retrieveAllBooks();
+    
+}
